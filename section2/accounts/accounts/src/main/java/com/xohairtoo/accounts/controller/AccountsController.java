05631af495f847709e06da0fb2610abd4b8a1dce
Zohair Ahmed <ahmedzo1@corp.onespan.com>
2024-07-31 11:57:16 -0400

    microservice learning first commit

---
+++ section2/accounts/accounts/src/main/java/com/xohairtoo/accounts/controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.xohairtoo.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("/sayHello")
+    public String syaHello(){
+        return "Hi from Zohair";
+    }
+}
