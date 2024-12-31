585edeab857af3400bef266b9487f6854855ff6b
Zohair Ahmed <ahmedzo1@corp.onespan.com>
2024-12-31 16:35:07 -0500

    added section6 for configuration management

---
+++ section6/v1-springboot/accounts/src/main/java/com/xohairtoo/accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,103 @@
+package com.xohairtoo.accounts.exception;
+
+import com.xohairtoo.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Handle MethodArgumentNotValidException
+     * @param ex - MethodArgumentNotValidException object
+     * @param headers - HttpHeaders object
+     * @param status - HttpStatusCode object
+     * @param request - WebRequest object
+     * @return - ResponseEntity with Map of validation errors
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> validationErrors = new HashMap<>();
+
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Handle CustomerAlreadyExistsException
+     * @param exception - CustomerAlreadyExistsException object
+     * @param webRequest - WebRequest object
+     * @return - ResponseEntity with ErrorResponseDto object
+     */
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
+                                                                                 WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Handle global exception
+     * @param exception - Exception object
+     * @param webRequest - WebRequest object
+     * @return - ResponseEntity with ErrorResponseDto object
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Handle ResourceNotFoundException
+     * @param exception - ResourceNotFoundException object
+     * @param webRequest - WebRequest object
+     * @return - ResponseEntity with ErrorResponseDto object
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                             WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+}
